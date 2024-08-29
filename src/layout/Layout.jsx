e338dab10f65ab0f8c1074f82d27432997d2956b
Shohanur-Shohan <shohanurshohan734@gmail.com>
2024-08-30 00:54:14 +0600

    footer done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
-import { Footer } from "../components/Home/Footer"
+// import { Footer } from "../components/Home/Footer"
 import Header from "../components/Home/Header"
+import Footer from "../components/Home/Footer"
 
 const Layout = () => {
   return (
