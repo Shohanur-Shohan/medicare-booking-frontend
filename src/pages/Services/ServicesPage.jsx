585ecc9eaa46583f96d1af39477a26673adbe747
Shohanur-Shohan <shohanurshohan734@gmail.com>
2024-09-23 22:25:49 +0600

    service page done

---
+++ src/pages/Services/ServicesPage.jsx
@@ -0,0 +1,11 @@
+import Services from "../../components/Home/Services";
+
+const ServicesPage = () => {
+  return (
+    <>
+      <Services />
+    </>
+  );
+};
+
+export default ServicesPage;
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,10 +1,8 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 import { Home } from "../pages/Home";
 import Contact from "../pages/Contact";
-import Services from "../pages/Services";
+import Services from "../pages/Services/ServicesPage";
 import { Doctors } from "../pages/Doctors/Doctors";
 import { DoctorDetails } from "../pages/Doctors/DoctorDetails";
 import { Login } from "../pages/Login";
@@ -15,45 +13,43 @@ export const Router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
-    errorElement: <Error/>,
+    errorElement: <Error />,
     children: [
-        {
-          path: "/",
-          errorElement: <Error/>,
-          element: <Home></Home>,
-        },
-        {
-          path: "/contact",
-          errorElement: <Error/>,
-          element: <Contact></Contact>,
-        },
-        {
-          path: "/services",
-          errorElement: <Error/>,
-          element: <Services></Services>,
-        },
-        {
-          path: "/doctors",
-          errorElement: <Error/>,
-          element: <Doctors></Doctors>,
-        },
-        {
-          path: "/doctors-details",
-          errorElement: <Error/>,
-          element: <DoctorDetails></DoctorDetails>,
-        },
-        {
-          path: "/login",
-          errorElement: <Error/>,
-          element: <Login></Login>,
-        },
-        {
-          path: "/register",
-          errorElement: <Error/>,
-          element: <SignUp></SignUp>,
-        },
-    ]
-      
+      {
+        path: "/",
+        errorElement: <Error />,
+        element: <Home></Home>,
+      },
+      {
+        path: "/contact",
+        errorElement: <Error />,
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/services",
+        errorElement: <Error />,
+        element: <Services></Services>,
+      },
+      {
+        path: "/doctors",
+        errorElement: <Error />,
+        element: <Doctors></Doctors>,
+      },
+      {
+        path: "/doctors-details",
+        errorElement: <Error />,
+        element: <DoctorDetails></DoctorDetails>,
+      },
+      {
+        path: "/login",
+        errorElement: <Error />,
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        errorElement: <Error />,
+        element: <SignUp></SignUp>,
+      },
+    ],
   },
-  
 ]);
